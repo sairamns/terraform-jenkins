bd970b3f5254bfaf472dc3621c5ccadcc3eb75d4
sairamns <129178979+sairamns@users.noreply.github.com>
2023-10-11 15:32:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,18 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test-instance" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    
+}
